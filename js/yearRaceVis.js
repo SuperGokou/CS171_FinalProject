1f015eeeaf0e28833cad01fa60eb2d7815b12f1e
Eagle-Space <124315051+Eagle-Space@users.noreply.github.com>
2023-11-19 18:55:08 -0700

    Update yearRaceVis.js

--- js/yearRaceVis.js
+++ js/yearRaceVis.js
@@ -9,7 +9,7 @@ class YearRaceVis {
     initVis() {
         let vis = this;
 
-        vis.margin = {top: 20, right: 20, bottom: 30, left: 40};
+        vis.margin = { top: 20, right: 20, bottom: 30, left: 40 };
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = 500 - vis.margin.top - vis.margin.bottom;
 
@@ -19,18 +19,16 @@ class YearRaceVis {
             .append("g")
             .attr("transform", `translate(${vis.margin.left},${vis.margin.top})`);
 
-        vis.color = d3.scaleOrdinal(d3.schemeCategory10); 
+        vis.color = d3.scaleOrdinal(d3.schemeCategory10);
 
         vis.wrangleData();
     }
 
     wrangleData() {
         let vis = this;
-    
-        // Group data by year and race
+
         let yearRaceMap = d3.rollup(vis.shootingData, v => v.length, d => d.date, d => d.race);
-    
-        // Process data for stacked bar chart
+
         vis.processedData = Array.from(yearRaceMap, ([year, races]) => {
             let raceCounts = { year };
             for (let [race, count] of races) {
@@ -38,33 +36,29 @@ class YearRaceVis {
             }
             return raceCounts;
         });
-    
+
         vis.updateVis();
     }
-    
 
     updateVis() {
         let vis = this;
 
-        // Define scales
         vis.x = d3.scaleBand()
-            .range([0, vis.width])
-            .padding(0.1);
+            .rangeRound([0, vis.width])
+            .paddingInner(0.05)
+            .align(0.1);
+
         vis.y = d3.scaleLinear()
-            .range([vis.height, 0]);
+            .rangeRound([vis.height, 0]);
 
-        // Define the stack generator
         vis.stack = d3.stack()
-            .keys(["White", "Black", "Asian", "Hispanic", "Other", "Unknown"]); 
+            .keys(["White", "Black", "Asian", "Hispanic", "Other", "Unknown"]);
 
-        // Stack data
         vis.series = vis.stack(vis.processedData);
 
-        // Set domains
         vis.x.domain(vis.processedData.map(d => d.year));
         vis.y.domain([0, d3.max(vis.series, d => d3.max(d, d => d[1]))]);
 
-        // Draw bars
         vis.svg.append("g")
             .selectAll("g")
             .data(vis.series)
@@ -82,8 +76,30 @@ class YearRaceVis {
             .attr("class", "axis")
             .attr("transform", `translate(0,${vis.height})`)
             .call(d3.axisBottom(vis.x));
+
         vis.svg.append("g")
             .attr("class", "axis")
             .call(d3.axisLeft(vis.y));
+
+        // Legend
+        let legend = vis.svg.selectAll(".legend")
+            .data(vis.color.domain().slice().reverse())
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", (d, i) => `translate(0,${i * 20})`);
+
+        legend.append("rect")
+            .attr("x", vis.width - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", vis.color);
+
+        legend.append("text")
+            .attr("x", vis.width - 24)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .style("fill", "white")
+            .text(d => d);
     }
 }
