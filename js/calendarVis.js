462398ff7beed362aef96b119d44ea3895ad4677
Ryan Henry <ryanhenry@college.harvard.edu>
2023-11-25 14:10:20 -0500

    Fix tooltip display errors

--- js/calendarVis.js
+++ js/calendarVis.js
@@ -15,17 +15,17 @@ class CalendarVis {
         let vis = this;
 
         // Create svg element
-        vis.margin = {top: 20, right: 20, bottom: 20, left: 20};
+        vis.margin = {top: 40, right: 20, bottom: 20, left: 20};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
-            .attr("width", vis.width)
-            .attr("height", vis.height)
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
             .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
 
         // Initialize calendar dimensions
-        vis.weekWidth = (vis.width) / 6.5;
+        vis.weekWidth = (vis.width) / 6;
         vis.dayWidth = vis.weekWidth / 7;
 
         // Create color scale
@@ -35,11 +35,9 @@ class CalendarVis {
             .clamp(true)
 
         // Initialize tooltip
-        vis.tooltip = d3.select("#" + vis.parentElement).append('div')
+        vis.tooltip = d3.select("body").append('div')
             .attr('class', "tooltip")
-            .style('opacity', 0)
-            .style("position", "absolute")
-            .style("pointer-events", "none");
+            .style('opacity', 0);
 
         // Initialize drawing interval
         d3.interval(
@@ -92,8 +90,8 @@ class CalendarVis {
             .attr("class", "key-rect")
             .attr("x", (d) => vis.getMonthXPos(d) + vis.weekWidth / 2)
             .attr("y", vis.height * 0.05)
-            .attr("width", vis.dayWidth)
-            .attr("height", vis.dayWidth)
+            .attr("width", vis.dayWidth * 1.1)
+            .attr("height", vis.dayWidth * 1.1)
             .attr("fill", (d) => ( d == 0 ? 'white' : vis.colorScale(d)))
             .attr("rx", 5)
 
@@ -102,11 +100,18 @@ class CalendarVis {
             .enter()
             .append("text")
             .attr("class", "key-text")
-            .attr("x", (d) => vis.getMonthXPos(d) + vis.weekWidth / 2 + vis.dayWidth / 2)
-            .attr("y", vis.height * 0.05 + vis.dayWidth / 1.4 )
-            .attr("text-anchor", "middle")
+            .attr("x", (d) => vis.getMonthXPos(d) + vis.weekWidth / 2 + vis.dayWidth / 4)
+            .attr("y", vis.height * 0.05 + vis.dayWidth / 1.2 )
+            .attr("text-anchor", "front")
             .attr("fill", (d) => d == 0 ? 'black' : 'white')
             .text((d) => (d == 3 ? '3+' : d))
+
+        vis.svg.append("text")
+            .attr("class", "key-label")
+            .attr("x", vis.width / 2)
+            .attr("y", vis.height * 0.05 - vis.dayWidth / 2)
+            .attr("fill", 'white')
+            .text("Number of Victims")
     }
 
     drawDay() {
@@ -155,14 +160,30 @@ class CalendarVis {
                 shootingInfo: shootingsToday
             })
             .on('mouseover', function (event, d) {
+                let tooltipWidth = vis.tooltip.node().getBoundingClientRect().width;
+                let tooltipHeight = vis.tooltip.node().getBoundingClientRect().height;
+                let pageX = event.pageX;
+                let pageY = event.pageY;
+                let margin = 10; // Margin from the cursor
+
+                // Calculate x position
+                let x = pageX + margin + tooltipWidth > window.innerWidth
+                    ? pageX - margin - tooltipWidth
+                    : pageX + margin;
+
+                // Calculate y position
+                let y = pageY + margin + tooltipHeight > window.innerHeight
+                    ? pageY - margin - tooltipHeight
+                    : pageY + margin;
+
                 vis.tooltip
                     .html(`
                      <h4>${vis.formatDate(d.date)}</h4>
                      <div><strong>Victims:</strong> ${d.victimCount}</div>
                     `)
                     .style('opacity', 1)
-                    .style('left', (event.pageX + 20) + 'px')
-                    .style('top', (event.pageY - vis.tooltip.node().getBoundingClientRect().height - 20) + 'px')
+                    .style('left', x + 'px')
+                    .style('top', y + 'px')
 
                 d3.select(this).attr('stroke', 'white')
             })
